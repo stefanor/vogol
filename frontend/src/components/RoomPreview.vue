a57dc5056d7bd632f1cc45f21d3848152ba26807
Stefano Rivera <stefanor@debian.org>
2020-08-14 16:06:43 -0700

    Display a warning on the mixed output when it is blanked
    Not blinking, like voctogui, but better than nothing.

--- frontend/src/components/RoomPreview.vue
+++ frontend/src/components/RoomPreview.vue
@@ -1,18 +1,32 @@
 <template>
   <div class="card">
-    <div class="card-header">Mixed Output</div>
+    <div class="card-header">
+      Mixed Output
+    </div>
     <div class="card-body">
+      <div class="stream-blanked badge badge-danger" v-if="blank">
+        Stream Blanked
+      </div>
       <VideoPreview room="room" />
     </div>
   </div>
 </template>
 
 <script>
+import {mapState} from 'vuex';
 import VideoPreview from './VideoPreview.vue';
 
 export default {
   components: {
     VideoPreview,
   },
+  computed: mapState({
+    blank: state => state.voctomix.stream_status != 'live',
+  }),
 };
 </script>
+<style>
+.stream-blanked {
+  position: absolute;
+}
+</style>
