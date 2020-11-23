0e5eb4fd68c97fb7788d3e231a27683cdc00982a
Stefano Rivera <stefanor@debian.org>
2020-11-23 13:18:32 -0800

    Merge AudioControl into VoctomixSource so the layout can be improved

--- frontend/src/components/VoctomixSource.vue
+++ frontend/src/components/VoctomixSource.vue
@@ -45,60 +45,128 @@
         </button>
       </li>
       <li class="list-group-item" v-if="!is_video_only">
-        <AudioControl v-bind:source="source" />
+        Audio:
+        <button
+          class="btn btn-danger"
+          v-bind:disabled="is_muted"
+          v-on:click="send('mute')"
+        >
+          Mute
+        </button>
+        <button
+          class="btn btn-success"
+          v-bind:disabled="is_unity"
+          v-on:click="send('unmute')"
+        >
+          0 db
+        </button>
+        <div class="volume">
+          <b-popover
+            v-bind:target="'volume-badge-' + source"
+            triggers="focus"
+            placement="top"
+            title="Fader (dB)"
+          >
+            <b-form-input
+              type="range"
+              v-model="volume_db"
+              step="0.5"
+              min="-20"
+              max="10"
+            ></b-form-input>
+          </b-popover>
+          <div
+            tabindex="0"
+            class="badge badge-info volume-badge"
+            v-bind:id="'volume-badge-' + source"
+            v-bind:class="[is_muted ? 'badge-danger' : 'badge-success']"
+          >
+            <b-icon-volume-mute-fill scale="1.5" v-if="is_muted" />
+            <b-icon-volume-up-fill scale="1.5" v-else />
+            {{ volume_db }} dB ({{ volume_percent }} %)
+          </div>
+        </div>
       </li>
     </ul>
   </div>
 </template>
 
 <script>
+import {
+  BFormInput,
+  BIconVolumeUpFill,
+  BIconVolumeMuteFill,
+  BPopover,
+} from 'bootstrap-vue';
 import startCase from 'lodash/startCase';
 import {mapState} from 'vuex';
 
-import AudioControl from './AudioControl.vue';
 import VideoPreview from './VideoPreview.vue';
 
 export default {
   props: ['source', 'index'],
   components: {
-    AudioControl,
+    BFormInput,
+    BIconVolumeUpFill,
+    BIconVolumeMuteFill,
+    BPopover,
     VideoPreview,
   },
-  computed: mapState({
-    is_fullscreen(state) {
-      return state.voctomix.composite_mode == 'fullscreen' && this.source_a;
-    },
-    is_fullscreen_solo(state) {
-      if (state.voctomix.composite_mode == 'fullscreen' && this.source_a) {
-        for (const [source, level] of Object.entries(state.voctomix.audio)) {
-          if (this.source == source) {
-            if (level < 0.2) {
-              return false;
-            }
-          } else {
-            if (level > 0.2) {
-              return false;
-            }
-          }
-        }
-        return true;
-      } else {
-        return false;
-      }
-    },
-    is_video_only(state) {
-      return state.voctomix.video_only_sources.indexOf(this.source) !== -1;
-    },
-    title() {
-      return startCase(this.source);
-    },
-    source_a(state) {
-      return state.voctomix.video_a == this.source;
-    },
-    source_b(state) {
-      return state.voctomix.video_b == this.source;
-    },
-  }),
+  computed: Object.assign(
+    mapState({
+      is_fullscreen(state) {
+        return state.voctomix.composite_mode == 'fullscreen' && this.source_a;
+      },
+      is_fullscreen_solo(state) {
+        return (
+          state.voctomix.composite_mode == 'fullscreen' &&
+          this.source_a &&
+          this.is_unity
+        );
+      },
+      is_muted() {
+        return this.volume < 0.2;
+      },
+      is_unity() {
+        return this.volume == 1;
+      },
+      is_video_only(state) {
+        return state.voctomix.video_only_sources.indexOf(this.source) !== -1;
+      },
+      title() {
+        return startCase(this.source);
+      },
+      source_a(state) {
+        return state.voctomix.video_a == this.source;
+      },
+      source_b(state) {
+        return state.voctomix.video_b == this.source;
+      },
+      volume(state) {
+        return state.voctomix.audio[this.source];
+      },
+      volume_percent() {
+        const volume_percent = Math.trunc(this.volume * 100);
+        return volume_percent;
+      },
+    }),
+    {
+      volume_db: {
+        get() {
+          const db = this.volume > 0 ? 20.0 * Math.log10(this.volume) : -20;
+          return Number.parseFloat(db).toFixed(1);
+        },
+        set(value) {
+          const volume = value > -20.0 ? 10 ** (value / 20) : 0;
+          this.$store.dispatch('voctomix_action', {
+            action: 'set_audio_volume',
+            source: this.source,
+            volume: volume,
+          });
+        },
+      },
+    }
+  ),
   methods: {
     fullscreen() {
       this.send('fullscreen');
@@ -156,5 +224,12 @@ export default {
 .source-b {
   background: #ff2222;
   color: white;
+}
+.volume {
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+}
+.volume-badge {
+  font-size: 1.2rem;
 }
 </style>
