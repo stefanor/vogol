7d4c4b1d238fb6860a0e56859253695cec455169
Stefano Rivera <stefanor@debian.org>
2020-12-11 16:15:36 -0800

    AJAX login form, so we can have some error handling

--- frontend/src/components/LoginModal.vue
+++ frontend/src/components/LoginModal.vue
@@ -2,12 +2,15 @@
   <b-modal
     title="Not Logged In"
     ok-title="Login"
+    v-bind:ok-disabled="authenticating"
     v-model="logged_out"
     v-on:ok="loginButton"
+    v-on:shown="modalShown"
     centered
     hide-header-close
     no-close-on-esc
     ok-only
+    static
   >
     <p>You need to login to use Vogol.</p>
     <p v-if="type == 'gitlab'">
@@ -15,12 +18,10 @@
       <a v-bind:href="url">{{ name }}</a
       >.
     </p>
-    <form
-      id="login-form"
-      method="POST"
-      action="/login"
-      v-if="type == 'password'"
-    >
+    <form ref="loginForm" v-if="type == 'password'">
+      <div class="alert alert-danger" v-if="error_message">
+        <b>Login Failed:</b> {{ error_message }}
+      </div>
       <div class="form-group">
         <input
           type="text"
@@ -36,9 +37,18 @@
           name="password"
           placeholder="Password"
         />
-        <input type="submit" hidden="hidden" />
       </div>
     </form>
+    <template #modal-ok>
+      <span
+        class="spinner-border spinner-border-sm"
+        role="status"
+        aria-hidden="true"
+        v-if="authenticating"
+      ></span>
+      <span v-if="authenticating">Logging in...</span>
+      <span v-else>Login</span>
+    </template>
   </b-modal>
 </template>
 
@@ -51,7 +61,9 @@ Vue.use(ModalPlugin);
 
 export default {
   computed: mapState({
+    error_message: state => state.auth.error_message,
     logged_out: state => state.websocket.connection == 'logged_out',
+    authenticating: state => state.auth.authenticating,
     name: state => state.auth.name,
     type: state => state.auth.type,
     url: state => state.auth.url,
@@ -63,8 +75,29 @@ export default {
         window.location = '/login';
       } else if (this.type == 'password') {
         event.preventDefault();
-        document.getElementById('login-form').submit();
+        this.passwordLogin();
       }
+    },
+    inputKeyDown(event) {
+      if (event.key == 'Enter' && this.type == 'password') {
+        event.preventDefault();
+        this.passwordLogin();
+      }
+    },
+    modalShown() {
+      if (this.type == 'password') {
+        const form = this.$refs.loginForm;
+        for (const input of form.querySelectorAll('input')) {
+          input.addEventListener('keydown', this.inputKeyDown);
+        }
+      }
+    },
+    passwordLogin() {
+      if (this.authenticating) {
+        return;
+      }
+      const data = new FormData(this.$refs.loginForm);
+      this.$store.dispatch('auth_password_login', data);
     },
   },
 };
